7c5b86d392254cc4cd2067762546781d1923a881
eugeniusz57 <104994034+eugeniusz57@users.noreply.github.com>
2022-09-05 17:36:33 +0200

    Change task 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,6 +4,7 @@ import Player from '@vimeo/player';
 const TIME_KYE = "videoplayer-current-time";
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
+let seconds = null;
 
 const onPlay = function(data) {
     localStorage.setItem(TIME_KYE, JSON.stringify(data));
@@ -13,10 +14,15 @@ player.on('timeupdate', throttle(onPlay, 1000));
     
 const obj = JSON.parse(localStorage.getItem(TIME_KYE));
 
-if(!obj){
-    const seconds = 0;
-    return
-}seconds = obj.seconds;
+isObject();
+
+function isObject(){
+    if(!obj){
+        seconds = 0;
+        return
+    }seconds = obj.seconds;
+}
+
  
 
 player.setCurrentTime(seconds);
