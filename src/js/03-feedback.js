39f10741d003ec17bcd608ed594941dd20a9a5c3
eugeniusz57 <104994034+eugeniusz57@users.noreply.github.com>
2022-09-07 18:14:26 +0200

    Control change

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,42 +5,44 @@ import { refs } from './03refs'
 const MESSAGE_KEY = "feedback-form-state";
 let  formData = {};
 
-refs.form.addEventListener('submit', onSubmit)
 refs.form.addEventListener('input',  throttle(onInputObject, 500))
+refs.form.addEventListener('submit', onSubmit)
 
 
+getMessage();
 
 function onInputObject(e){
     formData[e.target.name] = e.target.value;
-    console.log(formData);
 
     const message = JSON.stringify(formData);
         
     localStorage.setItem(MESSAGE_KEY, message );
 
 }
-getMessage();
+
 
 function onSubmit(e){
-e.preventDefault();
+    e.preventDefault();
+
     if(refs.inputEmail.value === "" || refs.textArea.value === "" ){
         alert("Enter all fields ");
-        return
     } 
 
-localStorage.removeItem(MESSAGE_KEY);
+    e.currentTarget.reset();
 
-e.currentTarget.reset();
+    localStorage.removeItem(MESSAGE_KEY);
+    getMessage();
 }
 
 function getMessage(){
 
     const isLocalStorage = JSON.parse(localStorage.getItem(MESSAGE_KEY));
-    console.log(isLocalStorage);
+
     if(isLocalStorage){
-        refs.textArea.value = isLocalStorage.message || '';
-        refs.inputEmail.value = isLocalStorage.email || '';
+        refs.textArea.value = isLocalStorage.message || "";
+        refs.inputEmail.value = isLocalStorage.email || "";
         formData = isLocalStorage;
-    }
+        return
+    } formData = {};
     
 }
